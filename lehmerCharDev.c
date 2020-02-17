9319d6e56331323b2a3a65ce243a0e9fee96215c
Björn Stresing <bjoern.stresing@gmx.de>
2020-02-17 19:42:54 +0100

    Add install, uninstall target; Provide infinite stream of chars

--- lehmerCharDev.c
+++ lehmerCharDev.c
@@ -1,12 +1,9 @@
-#include <linux/errno.h> /* EFAULT */
+#include <linux/errno.h>
 #include <linux/init.h>
 #include <linux/uaccess.h>
-#include <linux/fs.h> /* register_chrdev, unregister_chrdev */
-#include <linux/jiffies.h>
+#include <linux/fs.h>
 #include <linux/module.h>
 #include <linux/random.h>
-#include <linux/printk.h> /* printk */
-#include <uapi/linux/stat.h> /* S_IRUSR */
 #include "lehmer64.h"
 
 #define NAME "lehmer_character_device"
@@ -17,38 +14,35 @@ MODULE_DESCRIPTION("A character device that generates pseudo numbers using lehme
 
 static int major;
 
-static ssize_t read(struct file *filp, char __user *buf, size_t len, loff_t *off)
-{
-    size_t ret;
+static ssize_t read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
+    
+    size_t ret = 0;
     uint64_t rng = lehmer64();
-    //char kbuf[] = {'a', 'b', 'c', 'd'};
 
-    ret = 0;
-    if (*off == 0) {
-        if (copy_to_user(buf, &rng, sizeof(&rng))) {
-            ret = -EFAULT;
-        } else {
-            ret = sizeof(&rng);
-            *off = 1;
-        }
+    if (copy_to_user(buf, &rng, sizeof(&rng))) {
+        ret = -EFAULT;
+    } else {
+        ret = sizeof(&rng);
+        *off += ret;
     }
     return ret;
 }
 
 static const struct file_operations fops = {
+    
     .owner = THIS_MODULE,
     .read = read,
 };
 
-static int myinit(void)
-{
+static int myinit(void) {
+    
     lehmer64_seed(get_random_u64());
     major = register_chrdev(0, NAME, &fops);
     return 0;
 }
 
-static void myexit(void)
-{
+static void myexit(void) {
+    
     unregister_chrdev(major, NAME);
 }
 
